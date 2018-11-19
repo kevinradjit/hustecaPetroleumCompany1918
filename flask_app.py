a97b11394bba62385a3015b98f0061b13e4ed5e7
brentmusburger <ps0341957@gmail.com>
2018-11-19 18:28:49 +0000

    sovmestno nauchni gruppa

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, render_template, request, url_for
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config["DEBUG"] = True
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
@@ -11,8 +11,14 @@ SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostnam
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
+db = SQLAlchemy(app)
 comments = []
+class Comment(db.Model):
+
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
 
 @app.route("/", methods=["GET", "POST"])
 def index():
