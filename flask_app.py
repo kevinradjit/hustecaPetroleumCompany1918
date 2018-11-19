7dd0a95bf2a916f7e5503a3b3cbb20edf9e0c6a3
brentmusburger <ps0341957@gmail.com>
2018-11-19 16:32:04 +0000

    peut-etre ce genre de choses. est-ce que vous etes pret?

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,14 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+comments = []
 
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("main_page.html", comments=comments)
+
+    comments.append(request.form["contents"])
+    return redirect(url_for('index'))
