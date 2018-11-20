f652f3a39cd502611d42220d6dcbb84ffafbbb94
brentmusburger <ps0341957@gmail.com>
2018-11-20 00:28:54 +0000

    it is enough to write any program, but not easily

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,10 @@
+from datetime import datetime
 from flask import Flask, redirect, render_template, request, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask_login import current_user, login_required, login_user, LoginManager, logout_user, UserMixin
 from flask_migrate import Migrate
-from flask_login import login_required, login_user, LoginManager, logout_user, UserMixin
-from werkzeug.security import check_password_hash, generate_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash
+
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -19,17 +21,18 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-app.secret_key = "987987564654321321"
+app.secret_key = "9879865465432132132"
 login_manager = LoginManager()
 login_manager.init_app(app)
 
 
-class User(UserMixin):
+class User(UserMixin, db.Model):
 
-    def __init__(self, username, password_hash):
-        self.username = username
-        self.password_hash = password_hash
+    __tablename__ = "users"
 
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(128))
+    password_hash = db.Column(db.String(128))
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
@@ -38,16 +41,10 @@ class User(UserMixin):
     def get_id(self):
         return self.username
 
-all_users = {
-    "admin": User("admin", generate_password_hash("secret")),
-    "bob": User("bob", generate_password_hash("less-secret")),
-    "caroline": User("caroline", generate_password_hash("completely-secret")),
-}
-
 
 @login_manager.user_loader
 def load_user(user_id):
-    return all_users.get(user_id)
+    return User.query.filter_by(username=user_id).first()
 
 
 class Comment(db.Model):
@@ -56,6 +53,9 @@ class Comment(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4096))
+    posted = db.Column(db.DateTime, default=datetime.now)
+    commenter_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
+    commenter = db.relationship('User', foreign_keys=commenter_id)
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -63,7 +63,10 @@ def index():
     if request.method == "GET":
         return render_template("main_page.html", comments=Comment.query.all())
 
-    comment = Comment(content=request.form["contents"])
+    if not current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    comment = Comment(content=request.form["contents"], commenter=current_user)
     db.session.add(comment)
     db.session.commit()
     return redirect(url_for('index'))
@@ -75,16 +78,16 @@ def login():
     if request.method == "GET":
         return render_template("login_page.html", error=False)
 
-    username = request.form["username"]
-    if username not in all_users:
+    user = load_user(request.form["username"])
+    if user is None:
         return render_template("login_page.html", error=True)
-    user = all_users[username]
 
     if not user.check_password(request.form["password"]):
         return render_template("login_page.html", error=True)
 
     login_user(user)
     return redirect(url_for('index'))
+
 
 
 @app.route("/logout/")
